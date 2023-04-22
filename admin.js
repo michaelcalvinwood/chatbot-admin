ba731bfb7f5feae13598e4aa1eca40e583cb1dca
Michael Wood <michaelwood33311@icloud.com>
2023-04-22 13:21:20 -0400

    added receiving email, userName, and password during signup

--- admin.js
+++ admin.js
@@ -9,25 +9,35 @@ const cors = require('cors');
 const fs = require('fs');
 
 const app = express();
+
+app.use((req, res, next) => {
+    console.log(req);
+    next();
+})
+
 app.use(express.static('public'));
 app.use(express.json({limit: '200mb'})); 
 app.use(cors());
 
-app.get('/', (req, res) => {
+app.get('/yoyo', (req, res) => {
     res.send('Hello, World!');
 });
+
+
 
 app.post('/signup', (req, res) => {
     const { email, userName, password } = req.body;
 
     if (!email || !userName || !password) return res.status(400).json('bad request');
 
+    console.log(userName, email, password);
+
     /*
      * TODO: Add server-side validation of email, password, and username
      */
 
     res.status(200).json('ok');
-    
+
 })
 
 const httpsServer = https.createServer({
